e671ebc35aeab93d81dcaa935321926191170554
imfj007 <imfj007@gmail.com>
2026-03-14 14:18:43 +0500

    Updates

--- assets/Navbar-Bmw8HpN8.js
+++ assets/Navbar-Bmw8HpN8.js
@@ -1,4 +1,4 @@
-import{j as S,r as Be}from"./index-CoC7poI5.js";import{g as Ae,S as xe}from"./ScrollTrigger-0lJgtTtq.js";/*!
+import{j as S,r as Be}from"./index-DOnPRXBI.js";import{g as Ae,S as xe}from"./ScrollTrigger-0lJgtTtq.js";/*!
  * SplitText 3.12.7
  * https://gsap.com
  * 
